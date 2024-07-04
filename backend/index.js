39c3fcc22b85fa13b68ab5538a59999da9224835
Adan Asim <asimadan@gmail.com>
2024-07-04 15:17:07 +0500

    removed url

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-const uri = process.env.MONGODB_URI || "mongodb+srv://kigox11687:a6DriNXMwpcfcYBX@cluster0.ost8uo9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 console.log(uri)
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
